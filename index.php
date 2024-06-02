f717bea1bc21666d2d273598e68daa4a73bc0c2f
byassine <bouderaa.yassine@gmail.com>
2024-06-02 19:37:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+  <?php echo '<p>This final test for pipeline</p>'; ?> 
+  <?php echo '<p>Bye Bye</p>'; ?> 
+ </body>
+</html>
